90f59193f5930a963ddec9ea952fdd12f21c2d86
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2024-11-19 11:46:33 +0100

    Fix minor bug when loading pre-existing observed data with non-default filter names

--- src/process_fors2/photoZ/analysis.py
+++ src/process_fors2/photoZ/analysis.py
@@ -129,7 +129,7 @@ def load_data_for_run(inp_glob):
     else:
         from process_fors2.fetchData import readPZinputsHDF5
 
-        i_mag_ab, ab_colors, ab_cols_errs, z_specs = readPZinputsHDF5(f"pz_inputs_{os.path.basename(h5catpath)}", filt_names=None)
+        i_mag_ab, ab_colors, ab_cols_errs, z_specs = readPZinputsHDF5(f"pz_inputs_{os.path.basename(h5catpath)}", filt_names=filters_names)
 
     """old-fashioned way, deprecated, kept for reference only
     N_FILT = len(filters_arr) - 2
