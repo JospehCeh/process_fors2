4da43942e2c838b7160315d5aad06c65d2c151ba
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2025-01-17 18:47:53 +0100

    Improved BPT diagram classif and tables merging to account for no-FORS2 data.

--- src/process_fors2/analysis/galclassif.py
+++ src/process_fors2/analysis/galclassif.py
@@ -211,7 +211,7 @@ def Ke06_oi(log_oi_ha):
     return 1.18 * log_oi_ha + 1.30
 
 
-def bpt_classif(gelatoh5, xmatchh5, use_nc=False, return_dict=False):
+def bpt_classif(gelatoh5, xmatchh5, use_nc=False, return_dict=False, source="FORS2"):
     """
     Use Restframe Equivalent Widths from GELATO outputs to provide an rudimentary classification of galaxies, using BPT diagrams as described in
     [Kewley et al., 2006](https://ui.adsabs.harvard.edu/abs/2006MNRAS.372..961K/abstract).
@@ -226,7 +226,8 @@ def bpt_classif(gelatoh5, xmatchh5, use_nc=False, return_dict=False):
         Whether to use the 'NC' value for amibuous classifications instead of the highest score. The default is False.
     return_dict : bool, optional
         Whether to return the results as a dictionary (similar to `process_fors2.fetchData.gelato_xmatch_todict`) or a DataFrame. The default is False.
-
+    source : str, optional
+        Origin of the data : "FORS2", "GOGREEN" or "DESI". The default is "FORS2".
     Returns
     -------
     Object
@@ -235,7 +236,15 @@ def bpt_classif(gelatoh5, xmatchh5, use_nc=False, return_dict=False):
     from process_fors2.fetchData import readH5FileAttributes
 
     gelatout = readH5FileAttributes(gelatoh5)
-    xmatchout = readH5FileAttributes(xmatchh5)
+    if "fors2" in source.lower():
+        xmatchout = readH5FileAttributes(xmatchh5)
+    else:
+        xmatchout = pd.read_hdf(xmatchh5)
+        xmatchout = xmatchout.sort_values(by="num", ascending=True)
+        df_info_num = xmatchout["num"].values
+        key_tags = [f"SPEC{num}" for num in df_info_num]
+        xmatchout["name"] = key_tags
+        xmatchout.reset_index(drop=True, inplace=True)
     res_table = xmatchout.merge(right=gelatout, how="outer", on=["name", "num"])
     res_table["u-g"] = res_table["MAG_GAAP_u"] - res_table["MAG_GAAP_g"]
     res_table["r-i"] = res_table["MAG_GAAP_r"] - res_table["MAG_GAAP_i"]
