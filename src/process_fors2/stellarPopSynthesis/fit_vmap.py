da99e33f15c49c9c523aa9deefcd3251023206ad
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2025-02-21 20:38:00 +0100

    Fix the REW

--- src/process_fors2/stellarPopSynthesis/fit_vmap.py
+++ src/process_fors2/stellarPopSynthesis/fit_vmap.py
@@ -510,7 +510,7 @@ def calc_eqw(sur_wls, sur_spec, lin):
 
     line_wid = lin * 300 / C_KMS / 2
     cont_wid = lin * 1500 / C_KMS / 2
-    sur_flam = lsunPerHz_to_flam_noU(sur_wls, sur_spec, 0.0)
+    sur_flam = lsunPerHz_to_flam_noU(sur_wls, sur_spec, 0.001)
     nancont = jnp.where(jnp.logical_or(jnp.logical_and(sur_wls > lin - cont_wid, sur_wls < lin - line_wid), jnp.logical_and(sur_wls > lin + line_wid, sur_wls < lin + cont_wid)), sur_flam, jnp.nan)
     height = jnp.nanmean(nancont)
     vals = jnp.where(jnp.logical_and(sur_wls > lin - line_wid, sur_wls < lin + line_wid), sur_flam / height - 1.0, 0.0)
