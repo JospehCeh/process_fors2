63482bba94196201d35b7d895e4d86dd23ca1598
Joseph Chevalier <joe_ontheflex@hotmail.fr>
2025-03-12 17:33:15 +0100

    Modifications to the filter machanisms

--- src/process_fors2/photoZ/analysis.py
+++ src/process_fors2/photoZ/analysis.py
@@ -101,7 +101,7 @@ def load_data_for_run(inp_glob):
             # _filt = sedpyFilter(_fnumstr, _sedpyf.wavelength, _sedpyf.transmission)
         else:
             _f["path"] = os.path.abspath(_f["path"])  # os.path.abspath(os.path.join(DATALOC, _f["path"]))
-            _filt = observate.Filter(fnam, _f["path"])  # sedpyFilter(*load_filt(int(_fnumstr), _f["path"], _f["transmission"]))  # Could also use sedpy directly I think.
+            _filt = observate.Filter(fnam, directory=_f["path"])  # sedpyFilter(*load_filt(int(_fnumstr), _f["path"], _f["transmission"]))  # Could also use sedpy directly I think.
         filts_tup.append(_filt)
     filts_tup = tuple(filts_tup) + (NUV_filt, NIR_filt)
     # filts_tup = tuple(sedpyFilter(*load_filt(int(ident), filters_dict[ident]["path"], filters_dict[ident]["transmission"])) for ident in tqdm(filters_dict)) + (NUV_filt, NIR_filt)
